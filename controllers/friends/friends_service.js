226832e61cbbbf53e1069ea981fcde9fc8efb30b
divyesh306 <66518837+divyesh306@users.noreply.github.com>
2020-06-09 11:55:52 +0530

    initial commit

--- controllers/friends/friends_service.js
+++ controllers/friends/friends_service.js
@@ -1,30 +1,98 @@
 const Friends = require('./friends_model');
 
 exports.getAll = async function(){
-    var data  = await Friends
-    .find()
-    .populate('User','username')
-    .select('sender_id reciver_id');
+    try {
+         var data  = await Friends.find({})
+        .populate('sender_id',['username','email'])
+        .populate('reciver_id',['username','email'])
+        .exec();
+        return await data;
+    } catch (error) {
+        return error;
+    }   
+}
+exports.getSingeluser =async function(id){
+    try {
+        var data = await Friends.find({sender_id:id})
+        .populate('reciver_id',['username','email','image_url'])
+        .exec();
+        return await data;   
+    } catch (error) {
+        return (error);
+    }
+}
 
-    console.log("Friends : " ,data);
-
-    //  Friends.find();
+exports.getpendinglist = async function(id){
+    try {
+        var data = await Friends.find({sender_id:id , status : 'pending'})
+        .populate('reciver_id',['username','email','image_url'])
+        .exec();
+        return await data; 
+    } catch (error) {
+        return error;
+    }
+}
+exports.getacceptlist = async function(id){
+    try {
+        var data = await Friends.find({sender_id:id , status : 'accept'})
+        .populate('reciver_id',['username','email','image_url'])
+        .exec();
+        return await data; 
+    } catch (error) {
+        return error;
+    }
 }
 
 exports.new = async function (friendsdata){
-    if(await Friends.findOne({sender_id : friendsdata.sender_id , reciver_id:friendsdata.reciver_id})){
-        return await "You Both Are Already Friend"
-    }
-    else if(await Friends.findOne({sender_id : friendsdata.reciver_id , reciver_id:friendsdata.sender_id})){
-      return await "You Both Are Already Friend"
-    }
+    try {
+        if(await Friends.findOne({sender_id : friendsdata.sender_id , reciver_id:friendsdata.reciver_id})){
+            return await "You Both Are Already Friend"
+        }
+        else if(await Friends.findOne({sender_id : friendsdata.reciver_id , reciver_id:friendsdata.sender_id})){
+            return await "You Both Are Already Friend"
+        }
 
-    var friends = new Friends();
-    friends.sender_id = friendsdata.sender_id;
-    friends.reciver_id = friendsdata.reciver_id;
-    return await friends.save();
+        var friends = new Friends();
+        friends.sender_id = friendsdata.sender_id;
+        friends.reciver_id = friendsdata.reciver_id;
+        friends.status = friendsdata.status;
+        return await friends.save();
+    } catch (error) {
+        return error;
+    }
+   
+}
+
+exports.updateStatus = async function (id,friendsdata){
+    try {
+        var findfriends = await Friends.findById(id);
+        if(findfriends.status == friendsdata.status){
+            return await "You Both Are Already Friend"
+        };
+        
+        findfriends.status = friendsdata.status;
+        findfriends.save();
+        return await findfriends;
+        // if(findfriends == null){
+        //     var fd = await Friends.findById(id,function(err,fri){
+        //         if(err)
+        //             return err;
+
+        //     fri.sender_id = friendsdata.sender_id;
+        //     fri.status = friendsdata.status;
+        //     fri.save();
+        //     })  
+        //     return await fd;     
+        // }
+    } catch (error) {
+        return error;
+    }   
 }
 
 exports.remove = async function(id){
-    return await Friends.findByIdAndRemove(id);
+    try {
+         return await Friends.findByIdAndRemove(id);
+    } catch (error) {
+        return error;
+    }   
 }
