b1dd8b0f1679608b352863286025d317bd83cb80
divyesh306 <66518837+divyesh306@users.noreply.github.com>
2020-06-09 11:59:46 +0530

    initial commit

--- controllers/users/user_controller.js
+++ controllers/users/user_controller.js
@@ -2,24 +2,24 @@ const userService= require('./user_service');
 
 exports.getuser = function(req,res,next){
     userService.getAll()
-        .then(user => res.json(user))
-        .catch(err => next(err));
+        .then(user => res.json( user))
+        .catch(err => next({err : err}));
 }
 
 exports.getbyid = function(req,res,next){
     userService.getUserById(req.params.user_id)
-        .then(user => user ? res.json(user) : res.json("User Not Found"))
-        .catch(err=>next(err));
+        .then(user => user ? res.json(user) : res.json({msg : "User Not Found"}))
+        .catch(err=>next({err :err}));
 }
 
 exports._delete = function(req,res,next){
     userService.remove(req.params.user_id)
-        .then(user => res.json("User Deleted"))
-        .catch(err => next(err));
+        .then(user => res.json({msg : "User Deleted"}))
+        .catch(err => next({err :err}));
 }
 
 exports.create = function(req,res,next){
     userService.create(req.body)
-        .then(data => {res.json(data)})
-        .catch(err => next(err));
+        .then(users => {res.json(users)})
+        .catch(err => next({err :err}));
 }
