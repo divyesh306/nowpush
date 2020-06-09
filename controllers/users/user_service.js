71e55c3e5798a95d8f0febd64ebfc3b6ca716b41
divyesh306 <66518837+divyesh306@users.noreply.github.com>
2020-06-09 12:18:52 +0530

    initial commit

--- controllers/users/user_service.js
+++ controllers/users/user_service.js
@@ -1,55 +1,72 @@
 var User = require('./user_model');
 
-exports.login  = async function({username}){
-    
-}
-
 exports.create = async function(userdata) {
-    console.log("userdata : ",userdata.email);    
-    if(await User.findOne({ email : userdata.email })){
-        return "this email id is already registered";
-    };    
-    var random = parseInt(Math.random() * 1000000);
-	await User.find({
-		where:{
-			'my_id' : random
-		}
-	},function(err,data){
-		if (data.length == 0) {   
-            var user = new User();
-            user.username = userdata.username;
-            user.email = userdata.email;
-            user.device_token = userdata.device_token;
-            user.auth_token = userdata.auth_token;
-            user.my_id = random;
-            user.image_url = userdata.image_url;            
-            user.save();
-            console.log("User : ",user)
-        }
-        else{
-            create(userdata)
-        };
-    });
-}
+    try {
+        var userf = await User.findOne({ email : userdata.email });
+        if(userf != null){
+            var user = await User.findById(userf._id,function(err,users){
+                if (err)
+                    return err;
 
-exports.update = async function(id,userdata){
-    const user = User.findById(id);
-    if(!user){
-        throw "User Not Found";
-    }
-    userdata.device_token =  user.device_token.push(userdata.device_token);
-    Object.assign(user,userdata)
-    await user.save();
+                users.username = userdata.username;
+                users.device_token = userdata.device_token;
+                users.auth_token = userdata.auth_token;
+                users.image_url = userdata.image_url;            
+                users.save();
+            });    
+            var res = {
+                msg : "Login Success",
+                data : user
+            }
+            return await res;
+        };
+        if(userf == null){
+        var random = parseInt(Math.random() * 1000000);
+        let randomfind = await User.findOne({my_id : random});
+            if (randomfind == null) {
+                var user = new User();
+                user.username = userdata.username;
+                user.email = userdata.email;
+                user.device_token = userdata.device_token;
+                user.auth_token = userdata.auth_token;
+                user.my_id = random;
+                user.image_url = userdata.image_url;            
+                user.save();
+                var res = {
+                    msg : "Register Success",
+                    data : user
+                }
+                return await res;
+            }
+            else{
+                create(userdata)
+            }
+        }
+    } catch (error) {
+        return error;
+    }    
 }
 
 exports.getAll = async function(){
-    return await User.find();
+    try {
+        return await User.find();
+    } catch (error) {
+        return error;
+    }   
 }
 
 exports.getUserById = async function(id){
-    return await User.findById(id);
+    try {
+        return await User.findById(id);
+    } catch (error) {
+        return error;
+    }   
 }
 
 exports.remove = async function(id){
-    return await User.findByIdAndRemove(id);
+    try {
+        return await User.findByIdAndRemove(id);        
+    } catch (error) {
+        return error;
+    }
 }
